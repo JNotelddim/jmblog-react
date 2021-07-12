29631447fbb006ae97e5a41be369bbfabe053eaf
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-07-12 15:00:45 -0700

    Start an initial posts fetch hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
+// Modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+// Local components
 import ListPage from './component/page/ListPage';
 
+// Initializations
+const queryClient = new QueryClient();
+
+// App Providers Hierarchy
 ReactDOM.render(
   <React.StrictMode>
-    <ListPage />
+    <QueryClientProvider client={queryClient}>
+      <ListPage />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
