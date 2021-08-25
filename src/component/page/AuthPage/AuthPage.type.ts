490f6a0e0da5c8172aca3558f57a5a51d5f03337
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-08-25 12:48:35 -0700

    Remove replaced AuthPage component

--- src/component/page/AuthPage/AuthPage.type.ts
+++
@@ -1,7 +0,0 @@
-export const LOGIN = 'LOGIN';
-export const SIGNUP = 'SIGNUP';
-export type AuthPageType = typeof LOGIN | typeof SIGNUP;
-
-export interface AuthPageProps {
-  pageType: AuthPageType;
-}
