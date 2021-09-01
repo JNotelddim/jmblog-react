1fc1949092b6dd3463e7d562f40430663722ecb9
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-09-01 08:54:17 -0700

    Add container for items on list page

---
+++ src/component/page/ListPage/ListPage.style.tsx
@@ -0,0 +1,6 @@
+import { styled } from '@material-ui/core';
+
+export const ListContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+});
--- src/component/page/ListPage/ListPage.tsx
+++ src/component/page/ListPage/ListPage.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
-
-import { usePosts } from 'src/hook/api/posts';
-import CenteredLayout from 'src/component/layout/CenteredLayout';
 import { Link } from 'react-router-dom';
 
+// Components
+import CenteredLayout from 'src/component/layout/CenteredLayout';
+import { ListContainer } from './ListPage.style';
+
+// Hooks
+import { usePosts } from 'src/hook/api/posts';
+
+/**
+ * ListPage lists out summarized versions of the posts returned from the
+ * api. It is a public page, with some features only enabled for authenticated users.
+ */
 const ListPage = () => {
   const { data: posts, isLoading, isError } = usePosts({ enabled: true });
 
@@ -15,14 +23,15 @@ const ListPage = () => {
       </Typography>
       {isLoading && <Typography color="secondary"> Loading </Typography>}
       {isError && <Typography color="error"> Error </Typography>}
-      {!isLoading &&
-        !isError &&
-        posts &&
-        posts.map(({ id, title }) => (
-          <Link to={`/post/${id}`} key={id}>
-            {title}
-          </Link>
-        ))}
+      {!isLoading && !isError && posts && (
+        <ListContainer>
+          {posts.map(({ id, title }) => (
+            <Link to={`/post/${id}`} key={id}>
+              {title}
+            </Link>
+          ))}
+        </ListContainer>
+      )}
     </CenteredLayout>
   );
 };
