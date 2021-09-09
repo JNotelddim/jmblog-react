02744abc8be2b55c52a38d9cb1ad2d5cdbd715db
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-09-09 14:01:03 -0700

    Make Editor not crash (but no longer connected to form)

--- src/component/view/PostCreateView/PostCreateView.tsx
+++ src/component/view/PostCreateView/PostCreateView.tsx
@@ -1,5 +1,5 @@
 // Modules
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 // Hooks
@@ -10,7 +10,8 @@ import { Button } from '@material-ui/core';
 import { Editor } from 'react-draft-wysiwyg';
 import { Form, TextField, FormFooter } from './PostCreateView.style';
 
-// Types
+// Types + Classes
+import { EditorState } from 'draft-js';
 import { PostCreateData } from 'src/typings';
 
 // TODO: consider condensing this in with the Edit view if the typings
@@ -23,12 +24,19 @@ const PostCreateView: React.FC = () => {
     mode: 'onBlur',
   });
   const { isValid, errors } = formState;
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const editorRef = useRef(null);
 
   const onSubmit = () => {
     // TODO: useSavePost(formData);
   };
 
+  const onEditorChange = (stateUpdate: EditorState) => {
+    setEditorState(stateUpdate);
+  };
+
   // TODO: wysiwyg?
+  // TODO: fix wysiwyg connection: https://stackoverflow.com/questions/62176047/react-hook-form-controller-with-react-draft-wysiwyg
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -38,7 +46,14 @@ const PostCreateView: React.FC = () => {
         errorType={errors?.title}
       />
 
-      <Editor {...register('content', { required: true })} />
+      <Editor
+        ref={editorRef}
+        editorState={editorState}
+        onEditorStateChange={onEditorChange}
+        wrapperClassName="test-wrapper"
+        editorClassName="test-editor"
+        // {...register('content', { required: true })}
+      />
 
       <FormFooter>
         <Button>Cancel</Button>
