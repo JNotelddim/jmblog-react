53c21319b76a29e62d658dff6d69fbe05b85725d
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-10-20 13:13:37 -0700

    Fix up ListPage responsiveness

--- src/component/page/ListPage/ListPage.style.tsx
+++ src/component/page/ListPage/ListPage.style.tsx
@@ -1,4 +1,11 @@
 import { styled } from '@material-ui/core';
+import Text from 'src/component/base/Text';
+
+export const TitleText = styled((props) => (
+  <Text variant="h1" color="textSecondary" {...props} />
+))(({ theme }) => ({
+  paddingLeft: theme.spacing(3),
+}));
 
 export const ListContainer = styled('div')({
   display: 'flex',
--- src/component/partial/NavHeader/NavHeader.style.tsx
+++ src/component/partial/NavHeader/NavHeader.style.tsx
@@ -4,9 +4,15 @@ export const Container = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'space-between',
-  padding: theme.spacing(1.5, 10),
+  alignItems: 'center',
   minHeight: '60px', // stops it from jumping when there is/isn't a button in the header
   // TODO: ensure it stays this size
+
+  padding: theme.spacing(1.5, 3),
+
+  [theme.breakpoints.up('md')]: {
+    padding: theme.spacing(1.5, 10),
+  },
 }));
 
 export const IconButton = styled(MuiIconButton)({
